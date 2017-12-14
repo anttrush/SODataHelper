3191448d0dec327699b5473af85bf5319913c1dc
Anttrush <anttrush@users.noreply.github.com>
2017-12-14 13:24:20 +0800

    Update SaxHandler_QA.java
    ->
    private Connection con;
    private Statement stmt;
    private ResultSet rs;
    private PreparedStatement psql;
    private PreparedStatement psql2;
    private int i;
    ->
    null

--- SaxHandler_QA.java
+++ SaxHandler_QA.java
@@ -9,6 +9,10 @@ public class SaxHandler_QA extends DefaultHandler {
 private Connection con;
 private Statement stmt;
 private ResultSet rs;
+private PreparedStatement psql;
+private PreparedStatement psql2;
+private int i;
+
 	
 	public void initalMysql(Connection cc){
 		this.con = cc;
@@ -20,8 +24,8 @@ private ResultSet rs;
 	}
 	
 	public void closeMysql() throws SQLException{
-		this.con.close();
-		this.stmt.close();
+		con.close();
+		stmt.close();
 	}
 	
 	
@@ -67,8 +71,7 @@ private ResultSet rs;
 		// TODO Auto-generated method stub
 		        //遍历查询结果集
 		try {
-			PreparedStatement psql;
-			PreparedStatement psql2; 
+		if(arg2 == "row" && Integer.parseInt(arg3.getValue("Id")) > 108760){
 			//预处理添加数据，其中有两个参数--“？”
 			if (arg3.getValue("PostTypeId").equals("1")) { //ques
 				psql = con.prepareStatement("insert into questions (Id,CreationDate,AcceptedAnswerId,Score,ViewCount,TagId1,TagId2,TagId3,TagId4,TagId5,AnswerCount,CommentCount,FavoriteCount) "
@@ -86,7 +89,7 @@ private ResultSet rs;
 				ArrayList<String> tagNames = new ArrayList<String>();
 				ArrayList<Integer> tagIds = new ArrayList<Integer>();
 				char[] Tags = arg3.getValue("Tags").toCharArray(); // &lt;c#&gt;&lt;winforms&gt;&lt;type-conversion&gt;&lt;decimal&gt;&lt;opacity&gt;
-				int i = 0;
+				i = 0;
 				while (i < Tags.length){ // &lt;c#&gt;
 					i += 1;
 					String tagName = "";
@@ -99,7 +102,7 @@ private ResultSet rs;
 				}
 				for (String tagName : tagNames){
 					try{
-						stmt = con.createStatement(); //创建Statement对象
+						//stmt = con.createStatement(); //创建Statement对象
 			            String sql = "select Id from tags where TagName=\"" + tagName + "\"";    //检索tags表得到tag的Id
 			            rs = stmt.executeQuery(sql);//创建数据对象	
 			            rs.next();
@@ -123,6 +126,9 @@ private ResultSet rs;
 				}
 				System.out.println("ques\t"+arg3.getValue("Id") + "\t"+psql.executeUpdate());           //执行更新ques
 				System.out.println("post2Tag\t"+arg3.getValue("Id") + "\t"+psql2.executeUpdate());           //执行更新post2Tag
+				tagNames = null;
+				tagIds = null;
+				Tags = null;
 			}
 			else if (arg3.getValue("PostTypeId").equals("2")) { //ans
 				psql = con.prepareStatement("insert into answers (Id,CreationDate,ParentId,Score,CommentCount) "
@@ -136,10 +142,11 @@ private ResultSet rs;
 
 				ArrayList<Integer> tagIds = new ArrayList<Integer>();
 				try{
-					stmt = con.createStatement(); //创建Statement对象
+					//stmt = con.createStatement(); //创建Statement对象
 				    String sql = "select * from post2Tag where PostId=\"" + arg3.getValue("ParentId") + "\"";    //检索post2Tag表得到parent question
 				    rs = stmt.executeQuery(sql);//创建数据对象
-				    for (int i = 0;i < 5;i++){
+				    rs.next();
+				    for (i = 0;i < 5;i++){
 				    	tagIds.add(rs.getInt(i+1));
 				    }
 			        rs.close();
@@ -150,13 +157,15 @@ private ResultSet rs;
 				psql2 = con.prepareStatement("insert into post2Tag (PostId,TagId1,TagId2,TagId3,TagId4,TagId5) "
 						+ "values(?,?,?,?,?,?)");
 				psql2.setInt(1, Integer.parseInt(arg3.getValue("Id")));
-				for (int i = 0;i < 5;i++){
+				for (i = 0;i < 5;i++){
 					psql2.setInt(i+2, tagIds.get(i));
 				}
 				System.out.println("post2Tag\t"+arg3.getValue("Id") + "\t"+psql2.executeUpdate());           //执行更新post2Tag
+				tagIds = null;
 			}else{ // PostTypeId = 4 taginfomation
 				System.out.println(arg3.getValue("Id")+" : "+"Orther PostTypeId:"+arg3.getValue("PostTypeId"));
 			}
+		}//if Id > xxx
 		} catch(SQLException e) {
 			//数据库异常处理
 			e.printStackTrace(); 
